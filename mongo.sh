eaa1963f1fba88bb0631189907b2f886924b70c0
Nageswar5050 <nageswar0550@gmail.com>
2023-12-22 12:33:59 +0530

    initial commit

---
+++ mongo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+ID=$(id -u)
+DATEANDTIME=$(date +%F-%H:%M:%S)
+LOGFILE="/tmp/logs/$0/$DATEANDTIME.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+NC="\e[0m"
+
+echo "Script starting executing at $DATEANDTIME &>>LOGFILE 
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$Y You should run this using root$NC"
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$1.....$R FAILED$NC"
+        exit 1
+    else
+        echo -e "$1.....$G SUCCESS$NC"
+    fi
+}
+
+cp -u /home/centos/shell-script_practice/dependent_files/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE "Copyting mongo repo"
+
+dnf install mongodb-org -y
+
+VALIDATE "Installing mongo"
+
+systemctl enable mongod
+
+VALIDATE "Enabling mongod"
+
+systemctl enable start
+
+VALIDATE "Starting mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+VALIDATE "Changing local to global in mongo.conf"
+
+systemctl restart mongod
+
+VALIDATE "Restarting mongo"
