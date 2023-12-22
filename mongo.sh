c12b1f07d93f9801261ab8391a63d0d769940729
Nageswar5050 <nageswar0550@gmail.com>
2023-12-22 12:39:11 +0530

    initial commit

--- mongo.sh
+++ mongo.sh
@@ -25,26 +25,26 @@ VALIDATE(){
     fi
 }
 
-cp -u /home/centos/shell-script_practice/dependent_files/mongo.repo /etc/yum.repos.d/mongo.repo
+cp -u /home/centos/shell-script_practice/dependent_files/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE "Copyting mongo repo"
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOGFILE
 
 VALIDATE "Installing mongo"
 
-systemctl enable mongod
+systemctl enable mongod &>>$LOGFILE
 
 VALIDATE "Enabling mongod"
 
-systemctl enable start
+systemctl enable start &>>$LOGFILE
 
 VALIDATE "Starting mongod"
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOGFILE
 
 VALIDATE "Changing local to global in mongo.conf"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOGFILE
 
 VALIDATE "Restarting mongo"
